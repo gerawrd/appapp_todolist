bedb14990aee4264a15fd322997aeab6a7be02ec
abedejos-erika <ejabedejos@gmail.com>
2022-04-19 12:18:32 +0800

    login/register/logout/profiles

--- task/views.py
+++ task/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic.list import ListView
 from .models import Task
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-    
+
+@login_required
 def task(request):
     tasks = Task.objects.all()
     context = {
@@ -11,6 +13,7 @@ def task(request):
     }
     return render(request,'task/task_list.html', context)
 
+@login_required
 def task_detail_view(request, pk):
     task = get_object_or_404(Task, pk=pk)
     print(task)
